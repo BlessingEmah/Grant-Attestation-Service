d4859c6c51f6a26ac43c333555d04b34a34902d3
GigaHierz <lenahierzi@hotmail.de>
2023-08-07 03:10:21 +0200

    downgrade wagmi version

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from "next/app";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { optimism, optimismGoerli, baseGoerli } from "wagmi/chains";
 import celoGroups from "@celo/rainbowkit-celo/lists";
 import Layout from "../components/Layout";
@@ -11,31 +15,33 @@ import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
 
 const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string; // get one at https://cloud.walletconnect.com/app
 
-const { chains, publicClient } = configureChains(
+const { chains, provider } = configureChains(
   [optimism, optimismGoerli, baseGoerli],
   [publicProvider()]
 );
 
-const connectors = celoGroups({
-  chains,
+const { wallets } = getDefaultWallets({
+  appName: "Grant Attestation Service",
   projectId,
-  appName: (typeof document === "object" && document.title) || "Your App Name",
+  chains,
 });
 
-const appInfo = {
-  appName: "Celo Composer",
-};
+const connectors = connectorsForWallets([...wallets]);
 
-const wagmiConfig = createConfig({
+const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  publicClient: publicClient,
+  provider,
 });
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        appInfo={{ appName: "Grant Attestation Service" }}
+        coolMode={true}
+      >
         <Layout>
           <Component {...pageProps} />
         </Layout>
--- packages/react-app/pages/create-grant.tsx
+++ packages/react-app/pages/create-grant.tsx
@@ -1,16 +1,18 @@
 import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 import { useState } from "react";
-import { useWalletClient } from "wagmi";
+import { useSigner } from "wagmi";
+import { serialize, deserialize } from "wagmi";
 
 export const EASContractAddress = "0x1a5650d0ecbca349dd84bafa85790e3e6955eb84"; // GoerliOptimism v0.26
 
 export default function CreateApprovedGrantPage() {
-  const { data: walletClient } = useWalletClient();
+  const { data: signer } = useSigner();
 
   const [grantRecipient, setGrantRecipient] = useState(
     "0x99B551F0Bb2e634D726d62Bb2FF159a34964976C"
   );
   const [grantTitle, setGrantTitel] = useState("");
+  const [grantDescription, setGrantDescription] = useState("");
   const [bannerURL, setBannerURL] = useState("");
   const [startDate, setStartDate] = useState(0); // date => timestamp
   const [endDate, setEndDate] = useState(0);
@@ -18,49 +20,48 @@ export default function CreateApprovedGrantPage() {
   const [grantAmount, setGrantAmount] = useState(0); // eth => BigNumber???
 
   const eas = new EAS(EASContractAddress);
-  eas.connect(walletClient);
+  eas.connect(signer);
 
   const schemaEncoder = new SchemaEncoder(
-    "address grantRecipient,string grantTitle,string bannerURL,uint256 startDate,uint256 endDate,uint256 numberOfMilestones,uint256 grantAmount"
+    // "address grantRecipient,string grantTitle,string grantTitle,string bannerURL,uint256 startDate,uint256 endDate,uint256 numberOfMilestones,uint256 grantAmount"
+    "address grantRecipient,string grantTitle,string grantDescription"
   );
   const encodedData = schemaEncoder.encodeData([
     { name: "grantRecipient", value: grantRecipient, type: "address" },
     { name: "grantTitle", value: grantTitle, type: "string" },
-    { name: "bannerURL", value: bannerURL, type: "string" },
-    { name: "startDate", value: startDate, type: "uint256" },
-    { name: "endDate", value: endDate, type: "uint256" },
-    { name: "numberOfMilestones", value: numberOfMilestones, type: "uint256" },
-    { name: "grantAmount", value: grantAmount, type: "uint256" },
+    { name: "grantDescription", value: grantDescription, type: "string" },
+    // { name: "bannerURL", value: bannerURL, type: "string" },
+    // { name: "startDate", value: startDate, type: "uint256" },
+    // { name: "endDate", value: endDate, type: "uint256" },
+    // {
+    //   name: "numberOfMilestones",
+    //   value: numberOfMilestones,
+    //   type: "uint256",
+    // },
+    // { name: "grantAmount", value: grantAmount, type: "uint256" },
   ]);
 
   const schemaUID =
-    "0xca41e9d72f7190f0c47f590388930c46c4229f6284f4ca07d59e37f4d5df53e7";
+    // "0xca41e9d72f7190f0c47f590388930c46c4229f6284f4ca07d59e37f4d5df53e7";
+    "0x758055b1406ac4f866bbcc5a41c92910ec189bc44d275f7b284cec652026056a";
 
   const createAttestation = async () => {
-    const offchain = await eas.getOffchain();
-
-    const offchainAttestation = await offchain.signOffchainAttestation(
-      {
+    const attestation = await eas.attest({
+      schema: schemaUID,
+      data: {
         recipient: grantRecipient,
         // Unix timestamp of when attestation expires. (0 for no expiration)
-        /* eslint-disable-next-line */
         expirationTime: 0,
         // Unix timestamp of current time
-        /* eslint-disable-next-line */
-        time: 1671219636,
-        revocable: false,
-        version: 1,
-        /* eslint-disable-next-line */
-        nonce: 0,
-        schema: schemaUID,
+        revocable: true,
         refUID:
           "0x0000000000000000000000000000000000000000000000000000000000000000",
         data: encodedData,
       },
-      walletClient
-    );
+    });
+    const newAttestationUID = await attestation.wait();
 
-    console.log("New attestation UID:", offchainAttestation.uid);
+    console.log("New attestation UID:", newAttestationUID);
   };
 
   return (
@@ -102,6 +103,22 @@ export default function CreateApprovedGrantPage() {
         <div>
           <div className="mt-2 mr-4 flex flex-row ">
             <label className="block text-m mr-4 mt-2 leading-6 font-medium text-gray-900">
+              Grant Description
+            </label>
+            <input
+              id="grantDescription"
+              name="grantDescription"
+              type="text"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => setGrantDescription(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="mt-2 mr-4 flex flex-row ">
+            <label className="block text-m mr-4 mt-2 leading-6 font-medium text-gray-900">
               Banner URL
             </label>
             <input
@@ -126,7 +143,7 @@ export default function CreateApprovedGrantPage() {
               type="date"
               required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              onChange={(e) => setStartDate(e.target.value)}
+              onChange={(e) => setStartDate(new Date(e.target.value).getTime())}
             />
           </div>
         </div>
@@ -141,7 +158,7 @@ export default function CreateApprovedGrantPage() {
               type="date"
               required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              onChange={(e) => setEndDate(e.target.value)}
+              onChange={(e) => setEndDate(new Date(e.target.value).getTime())}
             />
           </div>
         </div>
@@ -157,7 +174,7 @@ export default function CreateApprovedGrantPage() {
               type="number"
               required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              onChange={(e) => setNumberOfMilestones(e.target.value)}
+              onChange={(e) => setNumberOfMilestones(Number(e.target.value))}
             />
           </div>
         </div>
@@ -172,18 +189,20 @@ export default function CreateApprovedGrantPage() {
               type="number"
               required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              onChange={(e) => setGrantAmount(e.target.value)}
+              onChange={(e) => setGrantAmount(Number(e.target.value))}
             />
           </div>
         </div>
       </form>
       <div>
-        <button
-          className="inline-flex w-60 justify-center rounded-full border px-5 my-5 py-2 text-md font-medium border-wood bg-gypsum text-black hover:bg-snow"
-          onClick={() => createAttestation()}
-        >
-          {"Create Attestation"}
-        </button>
+        {signer && (
+          <button
+            className="inline-flex w-60 justify-center rounded-full border px-5 my-5 py-2 text-md font-medium border-wood bg-gypsum text-black hover:bg-snow"
+            onClick={() => createAttestation()}
+          >
+            {"Create Attestation"}
+          </button>
+        )}
       </div>
     </div>
   );
